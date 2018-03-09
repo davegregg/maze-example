ab8b31187b33fd237aa4803e7b2d7da6cb44243a
davegregg@gmail.com <dmg@the-bridge.lan>
2018-03-08 20:14:28 -0500

    Finish checks and movement -- stopped mid-dev

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 {
 
-  const log = console.log
+  const log = (...args) => {
+    console.log(...args)
+    return args[ args.length - 1 ]
+  }
 
   const map =
 `▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
@@ -26,8 +29,10 @@
     start:      "S"   ,
     finish:     "F"   ,
   }
+  
+  const mapContainer = document.getElementById( "container" )
+  let player = null
 
-  const mapContainer = document.getElementById( "maze" )
 
   function findPropertyNameByValue( object, value ) {
     for ( let name of Object.getOwnPropertyNames( object ) )
@@ -37,9 +42,53 @@
     return property || null
   }
 
+
+  const offsetCoords = {
+    left: ( offset = 1 ) => [ 0, -offset ],
+    right: ( offset = 1 ) => [ 0, +offset ],
+    down: ( offset = 1 ) => [ +offset, 0 ],
+    up: ( offset = 1 ) => [ -offset, 0 ],
+  }
+
+
+  const check = {
+
+    next: offset => {
+      const nextCell = getNextCell( offset )
+      const nextIsWall = nextCell ? (nextCell.dataset.type === "wall") : true
+      const nextIsClear = !nextIsWall
+
+      return nextIsClear
+    },
+
+    left: () => log( "check left: ", check.next(offsetCoords.left) ),
+    right: () => log( "check right: ", check.next(offsetCoords.right) ),
+    down: () => log( "check down: ", check.next(offsetCoords.down) ),
+    up: () => log( "check up: ", check.next(offsetCoords.up) ),
+    
+  }
+
+
+  const move = {
+
+    next: offset => {
+      const nextCell = getNextCell( offset )
+      player.parentElement.removeChild( player )
+      setPlayerCoordinates( nextCell.dataset.row, nextCell.dataset.index )
+      return Boolean( nextCell.appendChild( player ) )
+    },
+
+    left: async () => move.next( offsetCoords.left ),
+    right: async () => move.next( offsetCoords.right ),
+    down: async () => move.next( offsetCoords.down ),
+    up: async () => move.next( offsetCoords.up ),
+    
+  }
+
+
   const make = {
 
-    map: () => {
+    map: async function () {
       map
         .split( tokens.delimeter )
         .forEach( make.row )
@@ -56,97 +105,119 @@
       mapContainer.appendChild( rowDiv )
     },
 
-    cell: ( rowDiv ) => {
+    cell: function ( rowDiv ) {
+
       callBackWithForEachCallback:
-      return ( cellToken, cellIndex ) => {
+      return function ( cellToken, cellIndex ) {
         const type = findPropertyNameByValue( tokens, cellToken )
   
         const cellDiv = document.createElement( "div" )
+        cellDiv.dataset.row = rowDiv.dataset.index
         cellDiv.dataset.index = cellIndex
         cellDiv.dataset.type = type
         cellDiv.classList.add( "cell", type )
-  
+        
         rowDiv.appendChild( cellDiv )
       }
     },
 
     player: () => {
-      const playerDiv = document.createElement( "div" )
-      playerDiv.id = "player"
-      playerDiv.classList.add( "cell" )
+      player = document.createElement( "div" )
+      player.id = "player"
+      player.classList.add( "cell" )
 
-      const start = document.querySelector(`div.row div.cell[data='${ tokens.start }']`)
-      start.appendChild(playerDiv)
+      const movePlayerToStart = startingCell => {
+        setPlayerCoordinates( startingCell.dataset.row, startingCell.dataset.index )
+        startingCell.appendChild( player )
+        return player
+      }
+      
+      return findCell({ type: "start" })
+        .then( movePlayerToStart )
     },
-
   }
+
+
+  function findCellByCoordinates ([ rowCoord, cellCoord ]) {
+    const beyondLeftOrTop = rowCoord < 0 || cellCoord < 0
+    const beyondRightOrBottom = 
+      rowCoord > mapContainer.childNodes.length || 
+      cellCoord > mapContainer.childNodes[0].childNodes.length
+
+    if ( beyondLeftOrTop || beyondRightOrBottom ) return null
+
+    return mapContainer
+      .childNodes[ rowCoord ]
+      .childNodes[ cellCoord ]
+  }
+
+
+  async function findCell ({ type, token }) {
+    const cellType = type ? type : findPropertyNameByValue( tokens, token )
+    return mapContainer.querySelector( `.cell[data-type='${ cellType }']` )
+  }
+
+
+  function calculateNewCoordinates ([ row, cellCoord ], [ rowOffset, cellOffset ]) {
+    return [ row + rowOffset, cellCoord + cellOffset ]
+  }
+
+
+  function getNextCell( getOffset ) {
+      const currentCoords = getPlayerCoordinates()
+      const nextCoords = calculateNewCoordinates( currentCoords, getOffset() )
+      return findCellByCoordinates( nextCoords )
+  }
+
+
+  function setPlayerCoordinates( row, cell ) {
+    return [ player.dataset.row = row, player.dataset.cell = cell ]
+  }
+
+
+  function getPlayerCoordinates() {
+    return [ Number( player.dataset.row ), Number( player.dataset.cell ) ]
+  }
+
+
+  function checkWin() {
+    const [ playerRow, playerCell ] = getPlayerCoordinates()
+
+    return findCell({ type: "finish" })
+      .then(({ finishingCellData }) => {
+        return (
+          finishingCellData.row == playerRow 
+          && finishingCellData.index == playerCell
+        )
+      })
+  }
+
 
   function keyHandler( event ) {
     const [ key, arrow ] = event.key.match( /Arrow(\w+)/ ) || []            // [ "ArrowDown", "Down" ]
     if ( !arrow ) return null
     
     const direction = arrow.toLowerCase()                                   // "down"
-    if (check[ direction ]())
+    if ( check[ direction ]() )
       move[ direction ]()
+        .then( checkWin )
   }
 
-  const deltas = {
-    left:  [ 0, -1 ],
-    right: [ 0, +1 ],
-    down:  [ +1, 0 ],
-    up:    [ -1, 0 ],
-  }
-
-  const check = {
-
-    left: ( deltas = deltas ) => {
-      return true
-    },
-    
-    right: ( deltas = deltas ) => {
-      return true
-    },
-    
-    down: ( deltas = deltas ) => {
-      return true
-    },
-    
-    up: ( deltas = deltas ) => {
-      return true
-    },
-    
-  }
-
-  const move = {
-
-    left: () => { 
-      log("left")
-    },
-
-    right: () => {
-      log("right")
-    },
-    
-    down: () => {
-      log("down")
-    },
-    
-    up: () => {
-      log("up")
-    },
-    
-  }
 
   function loadGame() {
-    document.addEventListener( "keydown", keyHandler )
     make.map()
+      .then(() => {
+        findCell({ type: "start" })
+          .then( element => startingCell = element )
+        findCell({ type: "finish" })
+          .then( element => finishingCell = element )
+        
+        make.player()
+      })
     
-    log( mapContainer.childNodes )
+    document.addEventListener( "keydown", keyHandler )
   }
 
   loadGame()
-
-  // TODO: place player cell
-  // TODO: write move.${direction} methods
 
 }
